e39ea3618254b4afabea545cfe6a446a707c095f
lexamulat <lexamulat@mail.ru>
2018-03-26 21:39:11 +0300

    1

--- main.cpp
+++ main.cpp
@@ -15,15 +15,26 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
        Widget w;
+       QTextStream cout (stdout);
+       setlocale(LC_ALL, "Russian");
+    ;
+
+        QString str = w.ui->owner->text();
+cout<<str;
+ cout<<"before";
        w.show();
 
 
 
+       cout<<"after";
+
+
+
+
+return a.exec();
 //QTextStream cout (stdout);
 //setlocale(LC_ALL, "Russian");
 //QString str="oleg";
 //cout<<str;
 
-
-return a.exec();
 }
